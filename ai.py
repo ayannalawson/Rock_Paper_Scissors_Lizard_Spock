d7d295a720c6dfb5d5f6605232afb414df4971f4
Ayaa <117710280+ayannalawson@users.noreply.github.com>
2023-03-07 02:20:12 -0500

    Add files via upload

---
+++ ai.py
@@ -0,0 +1,11 @@
+from player import Player
+import random
+
+class AI (Player):
+    def __init__(self):
+        super().__init__()
+        
+    def choose_gesture(self):
+        self.ai_turn = random.choice(["Rock", "Scissors", "Paper" , "Lizard", "Spock"])
+
+        print(f"The AI chose {self.ai_turn}")
